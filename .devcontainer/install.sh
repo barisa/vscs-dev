a24f06c253516e681a4ad0e9a30b96f4fe8cd650
Baris Ayaz <barisa@users.noreply.github.com>
2020-09-05 10:08:30 +0200

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/barisa/vscs-dev.git .
+git clone https://github.com/barisa/vscs-dev.git
 
 chmod +x .devcontainer/install-vscs.sh
 bash .devcontainer/install-vscs.sh
